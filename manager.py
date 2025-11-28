ccd8ef164b9b3dcfd238da5fbea0c6a829a484c0
Andrew Howard <thirtyfiveblack@gmail.com>
2025-11-29 07:55:51 +1100

    Update manager.py

--- manager.py
+++ manager.py
@@ -298,9 +298,9 @@ class CricketScoreboardPlugin(BasePlugin):
             away_team = next((c for c in competitors if c.get('homeAway') == 'away'), None)
             
             home_team_linescores = home_team.get('linescores', [])
-            home_team_batting = next((c for c in home_team_linescores if c.get('isBatting') == 'true'), None)
-            away_team_linescores = away_team.get('linescores', [])
-            away_team_batting = next((c for c in away_team_linescores if c.get('isBatting') == 'true'), None)
+            #home_team_batting = next((c for c in home_team_linescores if c.get('isBatting') == 'true'), None)
+            #away_team_linescores = away_team.get('linescores', [])
+            #away_team_batting = next((c for c in away_team_linescores if c.get('isBatting') == 'true'), None)
 
             if not home_team or not away_team:
                 return None
@@ -316,20 +316,20 @@ class CricketScoreboardPlugin(BasePlugin):
                     'abbrev': home_team.get('team', {}).get('abbreviation', 'UNK'),
                     #'score': int(home_team.get('score', 0)),
                     'score': home_team.get('score', 'Unknown'),
-                    'logo': home_team.get('team', {}).get('logo'),
-                    'wickets': home_team_batting.get('wickets',0),
-                    'runs': home_team_batting.get('runs',0),
-                    'overs': home_team_batting.get('overs',0)
+                    'logo': home_team.get('team', {}).get('logo')
+                    #'wickets': home_team_batting.get('wickets',0),
+                    #'runs': home_team_batting.get('runs',0),
+                    #'overs': home_team_batting.get('overs',0)
                 },
                 'away_team': {
                     'name': away_team.get('team', {}).get('displayName', 'Unknown'),
                     'abbrev': away_team.get('team', {}).get('abbreviation', 'UNK'),
                     #'score': int(away_team.get('score', 0)),
                     'score': away_team.get('score', 'Unknown'),
-                    'logo': away_team.get('team', {}).get('logo'),
-                    'wickets': away_team_batting.get('wickets',0),
-                    'runs': away_team_batting.get('runs',0),
-                    'overs': away_team_batting.get('overs',0)
+                    'logo': away_team.get('team', {}).get('logo')
+                    #'wickets': away_team_batting.get('wickets',0),
+                    #'runs': away_team_batting.get('runs',0),
+                    #'overs': away_team_batting.get('overs',0)
                 },
                 'status': {
                     'state': status.get('type', {}).get('state', 'unknown'),
