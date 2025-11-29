258f0667ebbfd56e4d7cda3476af1930ea7e6ef9
Andrew Howard <thirtyfiveblack@gmail.com>
2025-11-29 15:45:41 +1100

    Update manager.py

--- manager.py
+++ manager.py
@@ -329,9 +329,9 @@ class CricketScoreboardPlugin(BasePlugin):
                     #'score': int(away_team.get('score', 0)),
                     'score': away_team.get('score', 'Unknown'),
                     'logo': away_team.get('team', {}).get('logo'),
-                    'wickets': away_team_batting_one.get('wickets',0),
-                    'runs': away_team_batting_one.get('runs',0),
-                    'overs': away_team_batting_one.get('overs',0)
+                    'wickets': if away_team_batting_one: away_team_batting_one.get('wickets',0) else 0
+                    #'runs': away_team_batting_one.get('runs',0),
+                    #'overs': away_team_batting_one.get('overs',0)
                 },
                 'status': {
                     'state': status.get('type', {}).get('state', 'unknown'),
